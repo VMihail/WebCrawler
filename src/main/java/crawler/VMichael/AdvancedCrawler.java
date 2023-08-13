53c10671d3e9354ac5853743d7f27decd5c2c35a
VMihail <83347281+VMihail@users.noreply.github.com>
2023-08-14 00:45:56 +0300

    Init

--- src/main/java/crawler/VMichael/AdvancedCrawler.java
+++ src/main/java/crawler/VMichael/AdvancedCrawler.java
@@ -1,2 +1,20 @@
-package crawler.VMichael;public class AdvancedCrawler {
+package crawler.VMichael;
+
+import java.util.List;
+
+/**
+ * Crawls web sites.
+ *
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public interface AdvancedCrawler extends Crawler {
+  /**
+   * Downloads web site up to specified depth.
+   *
+   * @param url start <a href="http://tools.ietf.org/html/rfc3986">URL</a>.
+   * @param depth download depth.
+   * @param hosts domains to follow, pages on another domains should be ignored.
+   * @return download result.
+   */
+  Result download(String url, int depth, List<String> hosts);
 }
